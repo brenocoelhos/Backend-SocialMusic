57457626c5d5adc4808e2d7b9da54e46e87eb90e
brenocoelhos <brenocoelhos@gmail.com>
2025-10-30 16:09:01 -0300

    Add import_schema_web.php and update schema.sql

--- database/schema.sql
+++ database/schema.sql
@@ -1,26 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Tempo de geração: 30/10/2025 às 16:33
--- Versão do servidor: 10.4.32-MariaDB
--- Versão do PHP: 8.2.12
+
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Banco de dados: `socialmusic`
---
-
--- --------------------------------------------------------
 
 --
 -- Estrutura para tabela `avaliacoes`
